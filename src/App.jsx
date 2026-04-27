95696010a11566ae52b5bd84c86d60ca0228e01e
Christophe H <christophe@MacBook-Air-de-Christophe.local>
2026-04-27 08:16:14 +0200

    add the Vercel Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from './pages/Home'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'  // 添加这一行
 
 function App() {
   return (
     <>
       <Home />
       <Analytics />
+      <SpeedInsights />  {/* 添加这一行 */}
     </>
   )
 }
